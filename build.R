b28e600688f6b2685ce882522cdde09a78c157cd
Micah <micah.iserman@gmail.com>
2022-09-29 21:38:42 -0400

    updates repo list and capital_region view

--- build.R
+++ build.R
@@ -1,15 +1,17 @@
 library(community)
 
 datacommons_refresh("../social_data_commons")
-datacommons_map_files("../social_data_commons")
+datacommons_map_files("../social_data_commons", use_manifest = FALSE)
 
 datacommons_view(
   "../social_data_commons", "capital_region",
-  formatters = list(region_name = function(x) sub(",.*$", "", x))
+  formatters = list(region_name = function(x) sub(",.*$", "", x)),
+  prefer_repo = TRUE, use_manifest = FALSE
 )
 
 datacommons_view(
-  "../social_data_commons", "community_example", entity_info = NULL, prefer_repo = TRUE
+  "../social_data_commons", "community_example", entity_info = NULL,
+  prefer_repo = TRUE, use_manifest = FALSE
 )
 
 init_datacommons("../social_data_commons", serve = TRUE)
