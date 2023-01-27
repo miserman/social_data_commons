2e9b3118ae8662893aa93dac06498a00bcec3969
Micah <micah.iserman@gmail.com>
2023-01-27 14:38:00 -0500

    moves to sdc repositories

--- build.R
+++ build.R
@@ -1,17 +1,15 @@
 library(community)
 
 datacommons_refresh("../social_data_commons")
-datacommons_map_files("../social_data_commons", use_manifest = FALSE)
+datacommons_map_files("../social_data_commons")
 
 datacommons_view(
   "../social_data_commons", "capital_region",
-  formatters = list(region_name = function(x) sub(",.*$", "", x)),
-  prefer_repo = TRUE, use_manifest = FALSE
+  formatters = list(region_name = function(x) sub(",.*$", "", x))
 )
 
 datacommons_view(
-  "../social_data_commons", "community_example", entity_info = NULL,
-  prefer_repo = TRUE, use_manifest = FALSE
+  "../social_data_commons", "community_example", entity_info = NULL
 )
 
 init_datacommons("../social_data_commons", serve = TRUE)
